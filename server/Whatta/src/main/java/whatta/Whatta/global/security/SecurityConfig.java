eee8c601c75bdf38ffbf16df9977d68ad2ea0637
hyun <ehgus9755@naver.com>
2025-10-16 01:52:21 +0900

    feat: swagger 예외처리

--- server/Whatta/src/main/java/whatta/Whatta/global/security/SecurityConfig.java
+++ server/Whatta/src/main/java/whatta/Whatta/global/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session
                         -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/users/guest/login", "/api/auth/refresh").permitAll()
                         .anyRequest().authenticated()
                 )
