8bc9e2c5faf05cfcc934bd733e606a8fd3d32920
김민아 <143075401+alsdddk@users.noreply.github.com>
2025-10-16 20:53:40 +0900

    feat: 일간 조회에 인증 추가(2)

--- server/Whatta/src/main/java/whatta/Whatta/calendar/controller/CalendarViewController.java
+++ server/Whatta/src/main/java/whatta/Whatta/calendar/controller/CalendarViewController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +28,7 @@ public class CalendarViewController {
 
     @GetMapping
     @Operation(summary = "일간 조회", description = "해당 날짜의 일정과 작업을 조회합니다.")
-    public ResponseEntity<?> getDaily(@RequestParam LocalDate date) { //TODO: user 정보 함께 받아야 함
-        //임시 유저 정보
-        String userId = "user123";
+    public ResponseEntity<?> getDaily(@AuthenticationPrincipal String userId,  @RequestParam LocalDate date) {
         return Response.ok("success get events and tasks for " + date, calendarViewService.getDaily(userId, date));
     }
 }
