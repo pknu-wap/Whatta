9207ff7f693912a6a72942187e3cd248ecbe90fd
hyun <ehgus9755@naver.com>
2025-11-12 02:34:06 +0900

    feat: 버스Api 내부 Dto생성

---
+++ server/Whatta/src/main/java/whatta/Whatta/traffic/payload/response/BusStationResponse.java
@@ -0,0 +1,9 @@
+package whatta.Whatta.traffic.payload.response;
+
+public record BusStationResponse(
+        String busRouteId,
+        String busRouteNo,
+        String busStationName,
+        String busStationId
+
+) {}
