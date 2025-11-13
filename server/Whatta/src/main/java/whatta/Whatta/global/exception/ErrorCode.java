ac749b3c708df992b9dc9b3ab10e7ee38df4ce05
hyun <142883957+k-dhyun@users.noreply.github.com>
2025-11-13 17:29:29 +0900

    Revert "[feature]/#80-TrafficAPI 구현"

--- server/Whatta/src/main/java/whatta/Whatta/global/exception/ErrorCode.java
+++ server/Whatta/src/main/java/whatta/Whatta/global/exception/ErrorCode.java
@@ -12,7 +12,6 @@ public enum ErrorCode {
     TIME_ORDER_INVALID(HttpStatus.BAD_REQUEST, "602", "끝나는 시간이 시작 시간보다 앞서선 안됩니다."),
 
     TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "700", "해당 작업이 존재하지 않습니다."),
-    PUBLIC_API_FAILED(HttpStatus.BAD_REQUEST, "701", "API 호출 실패했습니다."),
 
     USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "800", "존재하지 않는 계정입니다."),
     USER_SETTING_NOT_FOUND(HttpStatus.NOT_FOUND, "801", "해당 계정의 설정이 존재하지 않습니다."),
