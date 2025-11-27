8841b09fe31c3bca711ca7f170e5d8170a4be251
이수민 <soomin1601@gmail.com>
2025-11-27 16:30:37 +0900

    design: 마이페이지 수정

--- client/Whatta/src/screens/MyPage/contants.tsx
+++ client/Whatta/src/screens/MyPage/contants.tsx
@@ -20,34 +20,34 @@ export const MY_SECTIONS: MySection[] = [
     title: '리마인드 알림 설정',
     data: [
       { key: '알림 시간 수정', route: 'NotifDefaults' },
-      { key: '소리 및 진동', route: 'UsageReminders' },
+      // { key: '소리 및 진동', route: 'UsageReminders' },
     ],
   },
   {
     title: '교통 알림 설정',
     data: [
       { key: '알림 시간 수정', route: 'TrafficAlertsRoot' },
-      { key: '소리 및 진동', route: 'Transitvibration' },
+      // { key: '소리 및 진동', route: 'Transitvibration' },
     ],
   },
   {
     title: '일정 요약 알림 설정',
     data: [
       { key: '알림 시간 수정', route: 'CalendarNotif' },
-      { key: '소리 및 진동', route: 'CalendarVibration' },
+      // { key: '소리 및 진동', route: 'CalendarVibration' },
     ],
   },
-  {
-    title: '메인화면 설정',
-    data: [
-      { key: '시작요일 설정', route: 'Preferences' },
-      { key: '시작 페이지 설정', route: 'FirstPages' },
-    ],
-  },
+  // {
+  //   title: '메인화면 설정',
+  //   data: [
+  //     { key: '시작요일 설정', route: 'Preferences' },
+  //     { key: '시작 페이지 설정', route: 'FirstPages' },
+  //   ],
+  // },
 
   // 작은 카드
-  {
-    size: 'small',
-    data: [{ key: '사용방법 안내', route: 'UsageReminders' }],
-  },
+  // {
+  //   size: 'small',
+  //   data: [{ key: '사용방법 안내', route: 'UsageReminders' }],
+  // },
 ]
