05b7729b6f6bfe2af33aec7917eee1367243e348
김민아 <143075401+alsdddk@users.noreply.github.com>
2026-02-23 17:27:27 +0900

    fix: validator에 month_last_day 추가

--- server/Whatta/src/main/java/whatta/Whatta/global/util/RepeatValidator.java
+++ server/Whatta/src/main/java/whatta/Whatta/global/util/RepeatValidator.java
@@ -18,22 +18,23 @@ public class RepeatValidator implements ConstraintValidator<ValidRepeat, RepeatR
             case DAY:
                 return repeatRequest.on() == null || repeatRequest.on().isEmpty();
             case WEEK:
-                if(repeatRequest.on() == null || repeatRequest.on().isEmpty())
+                if (repeatRequest.on() == null || repeatRequest.on().isEmpty())
                     return false;
-                for(String token : repeatRequest.on()) {
-                    if(token == null || !WEEK_DAY.matcher(token).matches()) {
+                for (String token : repeatRequest.on()) {
+                    if (token == null || !WEEK_DAY.matcher(token).matches()) {
                         return false;
                     }
                 }
                 return true;
             case MONTH:
-                if(repeatRequest.on() == null || repeatRequest.on().size() != 1)
+                if (repeatRequest.on() == null || repeatRequest.on().size() != 1)
                     return false;
                 String token = repeatRequest.on().get(0);
                 if (token == null) return false;
                 return MONTH_DAY.matcher(token).matches()
                         || MONTH_NTH.matcher(token).matches()
-                        || MONTH_LAST.matcher(token).matches();
+                        || MONTH_LAST.matcher(token).matches()
+                        || MONTH_LAST_DAY.matcher(token).matches();
         }
         return false;
     }
