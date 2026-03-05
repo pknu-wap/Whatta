46d74a6fb5de15c653430f254b370b8aa5e8720e
k-dhyun <ehgus9755@naver.com>
2026-03-06 01:05:59 +0900

    refactor: 오류 수정

--- server/Whatta/src/main/java/whatta/Whatta/traffic/client/bus/BusApiClient.java
+++ server/Whatta/src/main/java/whatta/Whatta/traffic/client/bus/BusApiClient.java
@@ -25,15 +25,15 @@ public class BusApiClient {
     private final ObjectMapper objectMapper;
 
     // 국토교통부_(TAGO)_버스도착정보
-    @Value("${public.data.bus.url}")
+    @Value("${public-data.bus-api.base-url}")
     private String arrivalBaseUrl;
 
     //국토교통부_(TAGO)_버스정류소정보
-    @Value("${public.data.bus.station.url}")
+    @Value("${public-data.bus-station-api.base-url}")
     private String stationBaseUrl;
 
     // 국토교통부_(TAGO) 서비스키
-    @Value("${public.data.bus.key}")
+    @Value("${public-data.bus-api.service-key}")
     private String serviceKey;
 
 
